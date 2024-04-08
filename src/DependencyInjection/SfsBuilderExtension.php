0a063092d161b6c425641efb168f0348938599b9
Javi H. Gil <javihgil@softspring.es>
2024-04-08 11:27:44 +0200

    Add welcome page listener

---
+++ src/DependencyInjection/SfsBuilderExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Softspring\SfsBuilderBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class SfsBuilderExtension extends Extension
+{
+    /**
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config/services'));
+        $loader->load('services.yaml');
+    }
+}
---
+++ templates/welcome.html.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Welcome to SfsBuilder!</title>
+</head>
+<body>
+<h1>Welcome to SfsBuilder</h1>
+</body>
+</html>
