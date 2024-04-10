71f9edb6a00a462d84efab9c5b452a19f15b368e
Javi H. Gil <javihgil@softspring.es>
2024-04-10 14:03:12 +0200

    Build assets

--- templates/welcome.html.php
+++ templates/welcome.html.php
@@ -1,8 +1,23 @@
 <html>
 <head>
     <title>Welcome to SfsBuilder!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<h1>Welcome to SfsBuilder</h1>
+<div class="px-4 pt-5 my-5 text-center border-bottom">
+    <h1 class="display-4 fw-bold">Welcome to SfsBuilder</h1>
+    <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc ultricies ultricies. Nullam nec purus nec nunc ultricies ultricies.</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+            <a href="/admin/cms/pages/create" type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Create first page</a>
+            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Documentation</button>
+        </div>
+    </div>
+    <div class="overflow-hidden" style="max-height: 30vh;">
+        <div class="container px-5">
+            <img src="" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+        </div>
+    </div>
+</div>
 </body>
 </html>
