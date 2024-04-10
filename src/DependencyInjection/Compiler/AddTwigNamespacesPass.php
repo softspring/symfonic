36c3228865f816d95f48479a3b1bb68b0ff1fe57
Javi H. Gil <javihgil@softspring.es>
2024-04-10 15:30:29 +0200

    Add admin_layout template

---
+++ src/DependencyInjection/Compiler/AddTwigNamespacesPass.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Softspring\SfsBuilder\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Finder\Finder;
+
+class AddTwigNamespacesPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $twigFilesystemLoaderDefinition = $container->getDefinition('twig.loader.native_filesystem');
+
+        // register project namespaces before collections to allow overriding
+        foreach ((new Finder())->in(__DIR__ . '/../../../templates/bundles')->depth(0)->directories() as $directory) {
+            $twigFilesystemLoaderDefinition->addMethodCall('prependPath', [$directory->getRealPath(), str_replace('Bundle', '', $directory->getBasename())]);
+        }
+    }
+}
--- src/SfsBuilderBundle.php
+++ src/SfsBuilderBundle.php
@@ -2,6 +2,8 @@
 
 namespace Softspring\SfsBuilder;
 
+use Softspring\SfsBuilder\DependencyInjection\Compiler\AddTwigNamespacesPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SfsBuilderBundle extends Bundle
@@ -9,5 +11,10 @@ class SfsBuilderBundle extends Bundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+    
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new AddTwigNamespacesPass());
     }
 }
