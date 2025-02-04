fc5277fa9c2ec2855b21376241358edce906e6da
Eva <eva@softspring.es>
2025-02-04 12:39:27 +0100

    Change welcome page (#31)

--- templates/welcome.html.php
+++ templates/welcome.html.php
@@ -4,19 +4,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<div class="px-4 pt-5 my-5 text-center border-bottom">
-    <h1 class="display-4 fw-bold">Welcome to Symfonic</h1>
-    <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc ultricies ultricies. Nullam nec purus nec nunc ultricies ultricies.</p>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-            <a href="/admin/cms/pages/create" type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Create first page</a>
-            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Documentation</button>
-        </div>
-    </div>
-    <div class="overflow-hidden" style="max-height: 30vh;">
-        <div class="container px-5">
-            <img src="" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
-        </div>
+<div class="px-4 pt-5 my-5 text-center">
+    <div class="col-12 col-lg-6 mx-auto text-start pb-5">
+        <h1 class="display-4 fw-bold">Welcome to Symfonic!</h1>
+        <p class="lead mb-5">Congratulations! Symfonic has been successfully installed and is ready to use.</p>
+        <div class="shadow rounded bg-light p-4 mb-5">
+
+            <p><strong>Documentation:</strong> Learn how to make the most of Symfonic with our <a href="https://symfonic.softspring.es/latest/">comprehensive guide</a>.</p>
+            <p class="mb-0"><strong>Commercial Support:</strong> Need personalized assistance? <a href="https://softspring.es/contact/">Get in touch with our team</a>.</p>
+
+        </div><p>Thank you for choosing Symfonic! We hope you enjoy building with our platform. 🚀</p>
     </div>
 </div>
 </body>
